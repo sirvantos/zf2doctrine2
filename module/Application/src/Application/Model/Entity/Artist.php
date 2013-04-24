d684e450f8bab7a3c7a09154bf215715e3c7ecf1
vladimir <sirvantos@gmail.com>
2013-04-24 15:34:17 +0400

    * add new mysql fields and indexes for album table

---
+++ module/Application/src/Application/Model/Entity/Artist.php
@@ -0,0 +1,69 @@
+<?php
+	namespace Application\Model\Entity;
+	use Doctrine\ORM\Mapping as ORM;
+	
+	/** 
+	 * @ORM\Entity 
+	 * @ORM\Table(
+	 *	name="artist", 
+	 *	indexes = {
+	 *		@ORM\Index(name="artist_name_idx", columns={"name"}), 
+	 *		@ORM\Index(name="artist_birthday_idx", columns={"birthday"})})
+	 */
+	class Artist {
+		/**
+		* @ORM\Id
+		* @ORM\GeneratedValue(strategy="AUTO")
+		* @ORM\Column(columnDefinition="integer unsigned")
+		*/
+		protected $id;
+
+		/** @ORM\Column(type="string", length=32, nullable=false) */
+		protected $name;
+		
+		/** @ORM\Column(type="datetime", nullable=false) */
+		protected $birthday;
+		
+		public function getId() 
+		{
+			return $this->id;
+		}
+		
+		/**
+		 * @return String
+		 */
+		public function getName() 
+		{
+			return $this->name;
+		}
+
+		/**
+		 * @param type $name
+		 * @return \Application\Entity\Artist
+		 */
+		public function setName($name) 
+		{
+			$this->name = $name;
+			
+			return $this;
+		}
+
+		/**
+		 * @return \DateTime
+		 */
+		public function getBirthday() 
+		{
+			return new \DateTime($this->birthday);
+		}
+
+		/**
+		 * @param String $title
+		 * @return \Application\Model\Entity\Artist
+		 */
+		public function setBirthday(\DateTime $birthday) 
+		{
+			$this->birthday = $birthday->format('Y-m-d H:i:s');
+			
+			return $this;
+		}
+	}
