46acfaa6cbd8e77e983397e2785291e6545f4481
vladimir <sirvantos@gmail.com>
2013-05-13 15:43:19 +0400

    * translate remove

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Zend\Log\Logger;
  */
 chdir(dirname(__DIR__));
 
-define(MUNEE_CACHE, realpath(__DIR__ . '/../data/cache/munee'));
+define('MUNEE_CACHE', realpath(__DIR__ . '/../data/cache/munee'));
 
 // Setup autoloading
 require 'init_autoloader.php';
