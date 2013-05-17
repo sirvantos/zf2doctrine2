b01ade31811e4f04c59dfb048ab19fb4f1673da7
vladimir <sirvantos@gmail.com>
2013-05-17 14:38:29 +0400

    * typo

--- config/application.config.php
+++ config/application.config.php
@@ -1,4 +1,8 @@
 <?php
+// application.config.php
+$configCacheEnabled = (
+	getenv('APPLICATION_ENVIRONMENT') == 'production'
+);
 
 return array(
     // This should be an array of module namespaces used in the application.
@@ -40,21 +44,21 @@ return array(
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        //'config_cache_enabled' => true,
+        'config_cache_enabled' => $configCacheEnabled,
 
         // The key used to create the configuration cache file name.
-        //'config_cache_key' => 'zf2_config_cache_key',
+        'config_cache_key' => 'zf2_config_cache_key',
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        //'module_map_cache_enabled' => true,
+        'module_map_cache_enabled' => $configCacheEnabled,
 
         // The key used to create the class map cache file name.
-        //'module_map_cache_key' => 'zf2_module_map_cache_key',
+        'module_map_cache_key' => 'zf2_module_map_cache_key',
 
         // The path in which to cache merged configuration.
-        //'cache_dir' => './data/cache',
+        'cache_dir' => './data/cache',
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
