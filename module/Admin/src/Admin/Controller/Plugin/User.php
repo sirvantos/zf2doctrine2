d3d3dde53df3089bf0e17552dd1b786285c666d1
vladimir <sirvantos@gmail.com>
2013-05-20 18:14:19 +0400

    * rename old class

---
+++ module/Admin/src/Admin/Controller/Plugin/User.php
@@ -0,0 +1,32 @@
+<?php
+	namespace Admin\Controller\Plugin;
+	
+	use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+	
+	/**
+	 * Description of User
+	 *
+	 * @author sirvantos
+	 */
+	final class User extends AbstractPlugin 
+	{
+		public function getListOfUsers()
+		{
+			
+		}
+		
+		public function addUser()
+		{
+			
+		}
+		
+		public function updateUser()
+		{
+			
+		}
+		
+		public function deleteUser()
+		{
+			
+		}
+	}
