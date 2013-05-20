6ea9f66ee3f8149496e9ab1953d95074b661f4f8
vladimir <sirvantos@gmail.com>
2013-05-20 18:08:20 +0400

    * remove old controller

--- module/Admin/src/Admin/Controller/UserController.php
+++ module/Admin/src/Admin/Controller/UserController.php
@@ -10,8 +10,7 @@
 	 */
 	use 
 		Zend\Mvc\Controller\AbstractActionController,
-		Zend\View\Model\ViewModel,
-		Admin\Form\Login as LoginForm;
+		Zend\View\Model\ViewModel;
 
 	/**
 	 * Default admin controller
@@ -23,48 +22,6 @@
 	{
 		public function indexAction()
 		{
-			if ($this->identity()) {
-				$this->
-					redirect()->
-						toRoute(
-							'admin/default', 
-							array('controller' => 'auth', 'action' => 'logout')
-						);
-			} else {
-				$this->
-					redirect()->
-						toRoute(
-							'admin/default', 
-							array('controller' => 'auth', 'action' => 'login')
-						);
-			}
-		}
-		
-		public function loginAction()
-		{
-			$request = $this->getRequest();
 			
-			$form = LoginForm::create('loginForm');
-			
-			$res = $this->auth()->login($form);
-			
-			if ($res === true)
-				$this->
-					redirect()->
-						toRoute('admin/default', array('controller' => 'news'));
-			
-			return new ViewModel(array('loginForm' => $form, 'result' => $res));
-		}
-		
-		public function logoutAction()
-		{
-			$this->auth()->logout();
-			
-			$this->
-				redirect()->
-					toRoute(
-						'admin/default', 
-						array('controller' => 'auth', 'action' => 'login')
-					);
 		}
 	}
